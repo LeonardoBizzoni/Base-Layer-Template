c273821009592cabb8f660db4ca8d6e9badfc283
LeonardoBizzoni <leo2002714@gmail.com>
2024-10-24 15:33:10 +0200

    Updated `base`

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "release" ]; then
     flags="-O3 -Wall -s -fno-exceptions"
 else
-    flags="-O0 -ggdb -fno-exceptions -Wno-cpp -fsanitize=address"
+    flags="-O0 -ggdb -fno-exceptions -Wno-cpp -fsanitize=address,undefined -g3"
 fi
 
 g++ src/main.cpp -o main $flags
