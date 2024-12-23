85e3dc27d4750cf410cb5a67d06662f03aa7576f
LeonardoBizzoni <leo2002714@gmail.com>
2024-12-23 08:16:12 +0100

    Updated build script and base

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 if [ "$1" = "release" ]; then
     flags="-Isrc/base -O3 -Wall -s -fno-exceptions"
 else
-    flags="-Isrc/base -O0 -ggdb -fno-exceptions \
-    -Wno-cpp -fsanitize=address,undefined -g3 -fvisibility=hidden"
+    flags="-Isrc/base -O0 -ggdb -fno-exceptions -Wno-cpp -fsanitize=address,undefined -g3 -fvisibility=hidden"
 fi
 
+# g++ src/main.cpp -o main $flags
 gcc src/main.c -o main $flags
