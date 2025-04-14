1a1cfed34e33f4ec31e63200b44e11b3087bb71b
LeonardoBizzoni <leo2002714@gmail.com>
2025-04-14 13:58:47 +0200

    Updated build script and `base`

--- build.sh
+++ build.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
+set -eu
 
-common_link="-lpthread -lm"
-common_flags="-Isrc/base -pedantic -Wall -Wno-cpp -Wno-unused-function"
-common_cpp="-std=c++23 -fno-exceptions"
-
-common_opt="-O3 -s"
-common_dbg="-O0 -g3 -fvisibility=hidden -DENABLE_ASSERT -DDEBUG"
-common_avr="-c -Os -ffunction-sections -fdata-sections"
-
-lnx_dbg="-ggdb -fsanitize=address,undefined"
-bsd_dbg="-ggdb"
-
-common_gui="-DOS_GUI"
-gui_x11="-D${os}_X11 -lX11 -lXext"
-gui_wayland="-D${os}_Wayland"
+file="main.c"
 
 for arg in "$@"; do
     if [[ $arg == *=* ]]; then
@@ -22,84 +10,152 @@ for arg in "$@"; do
         val="${arg#*=}"
         declare "$var"="$val"
     else
-	declare $arg='1'
+        declare $arg='1'
     fi
 done
 
-if [[ ! -v clang && ! -v avr ]]; then gcc=1;   fi
-if [ ! -v release ];             then debug=1; fi
+common_link="-lpthread -lm"
+common_flags="-pedantic -Wall -Wno-unused-function -Wno-gnu-anonymous-struct -Wno-nested-anon-types"
+common_cpp="-std=c++23 -fno-exceptions"
+common_gui="-DOS_GUI=1"
 
-# Compiler detection
-if [ -v gcc ];   then compiler=$([ -v cpp ] && echo "g++" || echo "gcc");         fi
-if [ -v clang ]; then compiler=$([ -v cpp ] && echo "clang++" || echo "clang");   fi
-if [ -v avr ];   then compiler=$([ -v cpp ] && echo "avr-g++" || echo "avr-gcc"); fi
+common_opt="-O3 -s"
+common_dbg="-O0 -g3 -fvisibility=hidden -DENABLE_ASSERT=1 -DDEBUG=1"
 
-# OS detection
-if [ ! -v avr ]; then
-    if [ "$(uname)" == "Linux" ]; then
-	os="LNX"
-	display_server=$(echo "$XDG_SESSION_TYPE")
-    elif [[ "$(uname)" == *"BSD"* ]]; then
-	os="BSD"
-	display_server=$(echo "$XDG_SESSION_TYPE")
-    elif [[ "$(uname)" == "Darwin" ]]; then
-	os="MACOS"
-    else
-	exit -1;
-    fi
+lnx_dbg="-ggdb -fsanitize=address,undefined"
+bsd_dbg="-ggdb"
+
+if [ ! -v release ]; then debug=1; fi
+
+if [ -v cross ]; then
+    case "${cross,,}" in
+        "atmega328p")
+            compiler=$([ -v cpp ] && echo "avr-g++" || echo "avr-gcc")
+            if [ ! -v clock ]; then clock=16000000; fi
+            flags="-Os -ffunction-sections -fdata-sections -mmcu=$cross -DF_CPU=$clock "
+
+            printf "+ [ AVR "
+            if [ -v cpp ]; then
+                flags+=$common_cpp
+                printf "C++ "
+            else
+                printf "C "
+            fi
+            printf "compilation ]\n"
+            [ -v debug ] && echo "+ [ debug mode ]" || echo "+ [ release mode ]"
+
+            (
+                set -x
+                $compiler $flags -c $file -o main.out
+                $compiler $flags main.out -o main.elf
+                avr-objcopy -O ihex -R .eeprom main.elf main.ihex
+            )
+            if [ ! -v noflash ]; then
+                if [ ! -v device ]; then
+                    echo "You didn't provide a device with \`device=/dev/something\`"
+                    exit -1
+                fi
+                (
+                    set -x
+                    sudo avrdude -F -V -c arduino -p atmega328p -P $device \
+                         -b 115200 -U flash:w:main.ihex
+                )
+            fi
+            ;;
+        "cortex-m7")
+            compiler=$([ -v cpp ] && echo "arm-none-eabi-g++" || echo "arm-none-eabi-gcc")
+
+            printf "+ [ ARM-NONE "
+            if [ -v cpp ]; then
+                flags+=$common_cpp
+                printf "C++ "
+            else
+                printf "C "
+            fi
+            printf "compilation ]\n"
+            [ -v debug ] && echo "+ [ debug mode ]" || echo "+ [ release mode ]"
+
+            (
+                set -x
+                $compiler $file -T stm32_linker.ld -o main.elf -mcpu=cortex-m7 -mthumb -nostdlib
+            )
+            if [ ! -v noflash ]; then
+                if [ ! -v device ]; then
+                    echo "You didn't provide a device with \`device=/dev/something\`"
+                    exit -1
+                fi
+                (
+                    set -x
+                )
+            fi
+            ;;
+        *)
+            exit -1
+            ;;
+    esac
 else
-    if [ ! -v board ]; then board="Arduino-Uno"; fi
-    case $board in
-	"Arduino-Uno")
-	    board="atmega328"
-	    if [ ! -v clock ]; then clock=16000000; fi
-	    ;;
-	*)
-	    exit -1
-	    ;;
+    if [ ! -v gcc ]; then clang=1; fi
+
+    if [ -v gcc ];   then compiler=$([ -v cpp ] && echo "g++" || echo "gcc");         fi
+    if [ -v clang ]; then compiler=$([ -v cpp ] && echo "clang++" || echo "clang");   fi
+
+    case "$(uname)" in
+        "Linux")
+            os="LNX"
+            if [ -v gui ]; then
+                display_server=$(echo "$XDG_SESSION_TYPE")
+            fi
+            ;;
+        *BSD*)
+            os="BSD"
+            if [ -v gui ]; then
+                display_server=$(echo "$XDG_SESSION_TYPE")
+            fi
+            ;;
+        "Darwin")
+            os="MACOS"
+            ;;
+        *)
+            exit -1
+            ;;
     esac
 
-    echo $clock
-    echo $board
-    common_avr+=" -mmcu=$board -DF_CPU=$clock"
-fi
+    flags="$common_flags $common_link "
+    if [ -v cpp ]; then flags+="$common_cpp "; fi
 
-flags="$common_flags "
-if [ -v cpp ]; then flags+="$common_cpp "; fi
-if [ -v avr ]; then
-    flags+="$common_avr "
-else
-    flags+="$common_link "
     if [ -v debug ]; then
-	flags+="$common_dbg "
-	if [ os == "LNX" ]; then
-	    flags+="$lnx_dbg "
-	elif [ os == "BSD" ]; then
-	    flags+="$bsd_dbg "
-	elif [ os == "MACOS" ]; then
-	    flags+="$macos_dbg "
-	fi
+        flags+="$common_dbg "
+        if [ "$os" == "LNX" ]; then
+            flags+="$lnx_dbg "
+        elif [ "$os" == "BSD" ]; then
+            flags+="$bsd_dbg "
+        elif [ "$os" == "MACOS" ]; then
+            flags+="$macos_dbg "
+        fi
     else
-	flags+="$common_opt "
+        flags+="$common_opt "
     fi
 
     if [ -v gui ]; then
-	flags+="$common_gui "
-	if [ os == "LNX" || os == "BSD" ]; then
-	    if [ "$display_server" == "x11" ]; then
-		flags+="$gui_x11 "
-	    else
-		flags+="$gui_wayland "
-	    fi
-	fi
+        flags+="$common_gui "
+        if [[ $os == "LNX" || $os == "BSD" ]]; then
+            if [ "$display_server" == "x11" ]; then
+                flags+="-D${os}_X11=1 -lX11 -lXext "
+            else
+                flags+="-D${os}_Wayland=1 "
+            fi
+        fi
+
+        if [ -v opengl ]; then
+            flags+="-lGL -lGLU -DUSING_OPENGL=1 "
+        fi
     fi
-fi
 
-if   [ -v gcc ];   then printf "+ [ GNU "
-elif [ -v clang ]; then printf "+ [ Clang "
-else                    printf "+ [ AVR "
-fi
+    if   [ -v gcc ];   then printf "+ [ GNU "
+    elif [ -v clang ]; then printf "+ [ Clang "
+    fi
 
-[ -v cpp ] && printf "C++ " || printf "C "; printf "compilation ]\n"
-[ -v debug ] && echo "+ [ debug mode ]" || echo "+ [ release mode ]"
-(set -x; $compiler $flags -o main src/main.c)
+    [ -v cpp ] && printf "C++ " || printf "C "; printf "compilation ]\n"
+    [ -v debug ] && echo "+ [ debug mode ]" || echo "+ [ release mode ]"
+    (set -x; $compiler $flags $file -o main)
+fi
